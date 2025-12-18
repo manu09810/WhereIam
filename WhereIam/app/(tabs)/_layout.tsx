e71de944b7feac71fc873986f6558136c857f314
manu09810 <manulorenzo19@gmail.com>
2025-12-18 00:59:14 -0300

    colors now are automatic

--- WhereIam/app/(tabs)/_layout.tsx
+++ WhereIam/app/(tabs)/_layout.tsx
@@ -26,7 +26,7 @@ export default function TabLayout() {
   const [tabBorderColor, setTabBorderColor] = useState("#dddddd");
 
   useEffect(() => {
-    const bg = averageColor || themeColors?.[0] || "#f7f7f7";
+    const bg = (themeColors && themeColors[1]) || averageColor || "#f7f7f7";
     const icon = getReadableTextColor(bg);
 
     setTabBarBackground(bg);
