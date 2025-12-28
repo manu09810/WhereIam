534e3e94f0cc9c06038a0d2bda5e4747bc3360d0
manu09810 <manulorenzo19@gmail.com>
2025-12-28 17:08:41 -0300

    add sounds for tabs when touching

--- WhereIam/components/haptic-tab.tsx
+++ WhereIam/components/haptic-tab.tsx
@@ -1,17 +1,22 @@
-import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
-import { PlatformPressable } from '@react-navigation/elements';
-import * as Haptics from 'expo-haptics';
+import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
+import { PlatformPressable } from "@react-navigation/elements";
+import { useAudioPlayer } from "expo-audio";
+import * as Haptics from "expo-haptics";
+const source = require("../assets/sounds/confirm-tap-394001.mp3");
 
 export function HapticTab(props: BottomTabBarButtonProps) {
+  const player = useAudioPlayer(source);
   return (
     <PlatformPressable
       {...props}
       onPressIn={(ev) => {
-        if (process.env.EXPO_OS === 'ios') {
+        if (process.env.EXPO_OS === "ios") {
           // Add a soft haptic feedback when pressing down on the tabs.
           Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
         }
         props.onPressIn?.(ev);
+        player.seekTo(0);
+        player.play();
       }}
     />
   );
